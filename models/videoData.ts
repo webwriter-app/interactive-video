2eecd5e035a12dc35759802840d82b7e8889bfaa
Andreas Dymek <andreas.dymek@rwth-aachen.de>
2025-03-17 19:08:20 +0100

    Removed old files from git

--- models/videoData.ts
+++
@@ -1,9 +0,0 @@
-export interface videoData {
-    isReplace: boolean;
-    startTime: number;
-    endTime?: number;
-    position?: { x: number; y: number };
-    content?: string;
-    size?: { width: number; height: number };
-    color?: string;
-  }
